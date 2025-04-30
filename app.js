c388f7be83d1b09edbf6241e1f75cc82a4a8583f
Robot <notarobot7811@gmail.com>
2025-04-30 15:47:36 -0500

    mostly done

--- app.js
+++ app.js
@@ -7,23 +7,25 @@ app.set('view engine', 'pug');
 const { data } = require('./data.json');  //all data
 const { projects } = data; //projects object only
 
-//access to the public files like public/
+//Access to the public files like public/
 app.use('/static', express.static(path.join(__dirname, 'public')));
 
 //Correction: remove trailing / on routes
 app.use((req, res, next) => {
     if (req.path !== '/' && req.path.endsWith('/')) {
-      const query = req.url.slice(req.path.length); // preserve query string
-    const cleanUrl = req.path.slice(0, -1) + query;
-    return res.redirect(301, cleanUrl);
+        const query = req.url.slice(req.path.length); // preserve query string
+        const cleanUrl = req.path.slice(0, -1) + query;
+        return res.redirect(301, cleanUrl);
     }
     next();
 });
 
+//---------//
 //  Routes 
+//---------//
 
 app.get('/', (req, res) => { 
-    res.render('index');
+    res.render('index', data);
 }); 
 
 app.get('/about', (req, res) => { 
@@ -40,22 +42,24 @@ app.get('/project/:id', (req, res) => {
     res.render('project', projectData);
 }); 
 
+//----------------//
 // Error Handling
-//Call 404
+//----------------//
+
+//Call 404 on unknown route
 app.use((req, res, next) => {
-    const err = new Error('not found 404');
-    // err.status = 404;
-    next(err)
+    const err = new Error('Oops... This page wasn\'t found!');
+    err.status = 404;
+    next(err) //pass to error handler
 })
 
-//prints the error
+//Error Handler
 app.use((err, req, res, next) => {
-    console.log(err)
-    // res.locals.error = err;
-    // res.status(err.status);
-    res.send('<h1>error</h1>');
-})
-
+    res.status(err.status || 500);
+    res.locals.message = err.message || 'Internal Server Error';
+    res.locals.status = err.status || 500;
+    res.render('error');
+});
 
 //Server location & response
 app.listen(3000, () => {
